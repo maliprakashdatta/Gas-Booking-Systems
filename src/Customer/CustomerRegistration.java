759ec34a4a8ee732ae656fce11a51bd68b40e551
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-06-07 09:29:22 +0530

    Customer Registration Form

---
+++ src/Customer/CustomerRegistration.java
@@ -0,0 +1,49 @@
+package Customer;
+
+public class CustomerRegistration {
+    private String FullName;
+//    private String Address;
+//    private int Mobile_no;
+//    private long DOB;
+//    private long Addhar_Number;
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String fullName) {
+        FullName = fullName;
+    }
+
+//    public String getAddress() {
+//        return Address;
+//    }
+//
+//    public void setAddress(String address) {
+//        Address = address;
+//    }
+//
+//    public int getMobile_no() {
+//        return Mobile_no;
+//    }
+//
+//    public void setMobile_no(int mobile_no) {
+//        Mobile_no = mobile_no;
+//    }
+//
+//    public long getDOB() {
+//        return DOB;
+//    }
+//
+//    public void setDOB(long DOB) {
+//        this.DOB = DOB;
+//    }
+//
+//    public long getAddhar_Number() {
+//        return Addhar_Number;
+//    }
+//
+//    public void setAddhar_Number(long addhar_Number) {
+//        Addhar_Number = addhar_Number;
+//    }
+}
